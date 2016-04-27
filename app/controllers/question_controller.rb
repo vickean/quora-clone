0138422750d1cff44afe15fdb7a8f44818a7e45c
vickean <vickean@gmail.com>
2016-04-27 18:16:32 +0800

    create question done. front page list of questions done

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,12 +1,18 @@
 get '/user/:user_id/question/new' do
-  erb :"static/questions/create"
+  erb :"static/questions/q_create"
 end
 
 post '/questions/create' do
   new_question = current_user.questions.build(params[:question])
   if new_question.save
-    redirect '/questions/saved'
+    redirect "/questions/#{new_question.id}"
   else
     redirect '/questions/notsaved'
   end
+end
+
+get '/questions/:question_id' do
+  this_question = Question.find(params[:question_id])
+  access_question(this_question)
+  erb :"static/questions/q_read"
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,7 @@ class Answer < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
 	belongs_to :user
 	belongs_to :question
+	validates :question_id, presence: true
+	validates :user_id, presence: true
+	validates :answer_content, presence: true
 end
